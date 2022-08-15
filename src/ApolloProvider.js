99f91e65c783a03c953833f9772359e4c27b0c9f
gabrielver <verronet.g@gmail.com>
2022-08-15 18:01:02 +0200

    adding posts

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,35 +1,3 @@
-// import React from "react";
-// import App from "./App";
-// import ApolloClient from "apollo-client";
-// import { InMemoryCache } from "apollo-cache-inmemory";
-// import { createHttpLink } from "apollo-link-http";
-// import { ApolloProvider } from "@apollo/react-hooks";
-// import { setContext } from "apollo-link-context";
-
-// const httpLink = createHttpLink({
-//   uri: "http://localhost:5000",
-// });
-
-// const authLink = setContext(() => {
-//   const token = localStorage.getItem("jwtToken");
-//   return {
-//     headers: {
-//       Authorization: token ? `Bearer ${token}` : "",
-//     },
-//   };
-// });
-
-// const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
-
-// export default (
-//   <ApolloProvider client={client}>
-//     <App />
-//   </ApolloProvider>
-// );
-
 import "./App.css";
 import {
   ApolloClient,
@@ -38,6 +6,7 @@ import {
   HttpLink,
   from,
 } from "@apollo/client";
+import { setContext } from "apollo-link-context";
 import { onError } from "@apollo/client/link/error";
 import App from "./App";
 
@@ -51,9 +20,18 @@ const errorLink = onError(({ graphqlErrors, networkError }) => {
 
 const link = from([errorLink, new HttpLink({ uri: "http://localhost:5000" })]);
 
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
+  link: authLink.concat(link),
   cache: new InMemoryCache(),
-  link: link,
 });
 
 export default (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useQuery, gql } from "@apollo/client";
-import { FETCH_POSTS_QUERY } from "../GraphQL/Queries";
+import { FETCH_POSTS_QUERY } from "../utils/graphql";
 import { Grid } from "semantic-ui-react";
+
+import { AuthContext } from "../context/auth";
 import PostCard from "../components/PostCard";
-import { useState } from "react";
+import PostForm from "../components/PostForm";
 
 function Home() {
+  const { user } = useContext(AuthContext);
+
   const { error, loading, data } = useQuery(FETCH_POSTS_QUERY);
   const [posts, setPosts] = useState([]);
 
@@ -23,6 +27,11 @@ function Home() {
         <h1>Recent Post</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm></PostForm>
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading posts ...</h1>
         ) : (
